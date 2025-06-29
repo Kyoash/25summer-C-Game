909c7c555925f3e0d356b3e5fb5684e13e966cff
kyoash <1670522132@qq.com>
2025-06-29 11:52:42 +0800

    Update: 完成基本gameviewmodel.h......

--- DongFangYongYe.pro
+++ DongFangYongYe.pro
@@ -68,3 +68,7 @@ HEADERS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    resources.qrc \
+    sources/ViewModel/resources.qrc
--- sources/ViewModel/gameviewmodel.h
+++ sources/ViewModel/gameviewmodel.h
@@ -1,10 +1,54 @@
+// gameviewmodel.h
 #ifndef GAMEVIEWMODEL_H
 #define GAMEVIEWMODEL_H
 
-class GameViewModel
+#include <QObject>
+#include "../model/gamemodel.h"
+
+class GameViewModel : public QObject
 {
+    Q_OBJECT
 public:
-    GameViewModel();
+    explicit GameViewModel(GameModel *model, QObject *parent = nullptr);
+
+    // 游戏控制
+    void initGame() {}
+    void startGame() {}
+
+    // 玩家控制
+    void setMoveLeft(bool move);
+    void setMoveRight(bool move);
+    void setMoveDown(bool move);
+    void setShooting(bool shooting);
+    void setSpeedShift(bool shift);
+    void toggleCollisionBoxes();
+
+    // 游戏状态访问
+    bool gameOver() const;
+    bool showCollisionBoxes() const;
+    int heroLife() const;
+
+    bool speedShift() const;
+    bool shooting() const;
+
+    // 游戏元素访问
+    QPixmap& getGamePixmap1();
+    QPixmap& getHeroPixmap();
+    std::pair<int, int> getHeroPosition();
+    QPixmap& getBossPixmap();
+    std::vector<QPixmap>& getEnemysPixmap();
+    std::pair<int, int> getBossPosition();
+    std::vector<std::pair<int, int>> getEnemysPosition(); // pending for enemy...
+    std::vector<std::pair<QPixmap*, std::pair<int, int>>>& getHeroBullets();
+    std::vector<std::pair<QPixmap*, std::pair<int, int>>>& getBossBullets();
+    std::vector<std::pair<QPixmap*, std::pair<int, int>>>& getEnemyBullets(); // pending for enemy...
+    //Bomb* bombs() { return m_model->bombs(); }
+
+signals:
+    void updateView();
+
+private:
+    GameModel *m_model;
 };
 
 #endif // GAMEVIEWMODEL_H
